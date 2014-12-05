86f75f0a9ed60a04f28e03626196ed87fc4ab06b
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-12-05 14:55:05 +0000

    Update bezier.js

--- bezier.js
+++ bezier.js
@@ -1,7 +1,35 @@
-var c = document.getElementById("my-canvas");
+$(document).ready(function() {
 
-var ctx = c.getContext("2d");
-
-$("#my-canvas").click(function(){
+	var c = document.getElementById("my-canvas");
+	var ctx = c.getContext("2d");
+	
+	var curveCoords = {
+		start: {x: 0, y:0},
+		pointOne: {x: 0, y: 0},
+		pointTwo: {x: 0, y: 0},
+		end: {x: 0, y: 0}
+	};
+	
+	function drawBezier(){
+		ctx.beginPath();
+		ctx.moveTo(curveCoords.start.x, curveCoords.start.y,);
+		ctx.bezierCurveTo(
+				curveCoords.pointOne.x, curveCoords.pointOne.x,
+				curveCoords.pointTwo.x, curveCoords.pointTwo.y,
+				curveCoords.end.x, curveCoords.end.y,);
+		ctx.stroke();
+	}
+	
+	$("#my-canvas").click(function(e){
+		var coord = $("#item").val();
+		var offset = $(this).offset();
+		var x = e.clientX - offset.left;
+		var y = e.clientY - offset.top;
+		
+		curveCoords[coord].x = x;
+		curveCoords[coord].y = y;
+		
+		drawBezier();
+	});
 
 });
